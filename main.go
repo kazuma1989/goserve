4d3839cf6ab91bf8ad54b19fbf6cf3508a7595f9
kazuma1989 <funifuni.1204@gmail.com>
2018-09-10 14:17:05 +0900

    feature: Set proper content type for JSON files.

--- main.go
+++ main.go
@@ -3,9 +3,24 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
 )
 
+type jsonHandler struct {
+	defaultHandler http.Handler
+}
+
+func (h *jsonHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if strings.HasSuffix(r.URL.Path, ".json") {
+		w.Header().Set("Content-Type", "application/json")
+	}
+
+	h.defaultHandler.ServeHTTP(w, r)
+}
+
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./")))
+	h := &jsonHandler{http.FileServer(http.Dir("./"))}
+	http.Handle("/", h)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
