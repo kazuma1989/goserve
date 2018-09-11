9ff198cb7de5b92f35ae44b423056c2f14cf48c4
kazuma1989 <funifuni.1204@gmail.com>
2018-09-12 07:57:20 +0900

    feature: Use goserve.json as the config instead of routes.json.

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"regexp"
+	"strconv"
 	"strings"
 	"time"
 
@@ -21,6 +22,11 @@ var (
 type jsonHandler struct {
 	defaultHandler http.Handler
 	routes         map[*regexp.Regexp]string
+}
+
+type goserveConfig struct {
+	Port   uint16
+	Routes map[string]string
 }
 
 func (h *jsonHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -49,15 +55,15 @@ func main() {
 	fmt.Printf("goserve %s", Version)
 	fmt.Println()
 
-	var data map[string]string
-	if raw, err := ioutil.ReadFile("./routes.json"); err == nil {
-		json.Unmarshal(raw, &data)
+	var config goserveConfig
+	if raw, err := ioutil.ReadFile("./goserve.json"); err == nil {
+		json.Unmarshal(raw, &config)
 	} else {
 		log.Println(err)
 	}
 
 	routes := make(map[*regexp.Regexp]string)
-	for k, v := range data {
+	for k, v := range config.Routes {
 		r, err := regexp.Compile(k)
 		if err == nil {
 			routes[r] = v
@@ -70,13 +76,19 @@ func main() {
 	}
 	http.Handle("/", h)
 
+	var port string
+	if config.Port != 0 {
+		port = strconv.Itoa(int(config.Port))
+	} else {
+		port = "8080"
+	}
 	browserOpen := time.AfterFunc(500*time.Millisecond, func() {
-		browser.OpenURL("http://localhost:8080")
+		browser.OpenURL("http://localhost:" + port)
 	})
 
 	err := make(chan error)
 	go func() {
-		err <- http.ListenAndServe(":8080", nil)
+		err <- http.ListenAndServe(":"+port, nil)
 	}()
 
 	log.Fatal(<-err)
